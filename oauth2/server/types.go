916ce12efa02feeeeb89e43831476dea64c26182
Erik Serrano <erik.serrano@nielsen.com>
2017-04-04 09:53:10 -0500

    format & document

--- oauth2/server/types.go
+++ oauth2/server/types.go
@@ -1,18 +1,16 @@
-// Paquete encargado de obtener un token de Google en base a un código de autorización
-// Documentación: https://developers.google.com/accounts/docs/OAuth2WebServer 
+// Package server makes request to Google API Oauth (https://developers.google.com/accounts/docs/OAuth2WebServer)
 package server
 
-// Estructura encargada de almacenar código de respuesta de Google
+// OAuth2WebServer represents the Google response
 type OAuth2WebServer struct {
-    Code         string
-	JsonResponse map[string]interface{}
+	Code         string
+	JSONResponse map[string]interface{}
 }
 
-
-// Método encargado de consultar una propiedad en la respuesta de Google
+// QueryProperty queries properties in the response
 func (oauth *OAuth2WebServer) QueryProperty(property string) interface{} {
-	if len(oauth.JsonResponse) > 0 {
-		if val, ok := oauth.JsonResponse[property]; ok == true {
+	if len(oauth.JSONResponse) > 0 {
+		if val, ok := oauth.JSONResponse[property]; ok == true {
 			return val
 		}
 	}
